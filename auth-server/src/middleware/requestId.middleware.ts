c9cd26b0184d2c7e442e2e82dbb1b5fa64e59530
Nand Patel <130226201+nand2384@users.noreply.github.com>
2026-06-12 23:02:27 +0530

    Delete auth-server directory

--- auth-server/src/middleware/requestId.middleware.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import crypto from "crypto";
-
-export default function requestId(req: Request, res: Response, next: NextFunction): void {
-    (req as Request).requestId = crypto.randomUUID();
-    next();
-};
--- auth-server/src/middleware/requestLogger.middleware.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default function requestLogger(req: Request, res: Response, next: NextFunction): void {
-    console.log(`[${req.requestId}] [REQUEST] ${req.method} ${req.originalUrl}`);
-
-    next();
-};
